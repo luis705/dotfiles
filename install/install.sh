86ace4ca636d164e45613900ea812907796514c5
Luís Otávio Lopes Amorim <luisotaviolamorim@gmail.com>
2021-03-20 22:53:10 -0300

    Initial files added

---
+++ install/install.sh
@@ -0,0 +1,25 @@
+# Instalando programas e bibliotecas
+sudo pacman -S --noconfirm - < pacman_all.txt
+pip3 install -r pip_install.txt
+
+# Configurações básicas
+cp ../.bashrc ~/
+cp ../.profile ~/
+cp ../.gitconfig ~/
+cp ../.gtkrc-2.0 ~/
+cp ../.Xresources ~/
+
+# Temas
+cd -R ../.themes ~/
+cp -R ../.icons ~/
+
+# Pasta config
+cp -r .config/* ~/.config
+
+# Wallpapers
+cp ../wallpapers ~/
+
+
+# Utilitarios
+cp -r ../Utils ~/
+cp xfce4-sccreenshoter ~/
---
+++ update.sh
@@ -0,0 +1,27 @@
+cp ~/.bashrc .
+cp ~/.profile .
+cp ~/.gitconfig .
+cp ~/.gtkrc-2.0 .
+cp ~/.Xresources .
+
+# Temas
+cp -R ~/.themes .
+rm -rf .themes/gruvbox-gtk/.git
+cp -R ~/.icons .
+rm -rf .icons/gruvbox-dark-icons-gtk/.git
+
+# Config
+cp -r ~/.config/i3 .config/
+cp -r ~/.config/rofi .config/
+cp -r ~/.config/terminator .config/
+
+# Utils
+cp  ~/Utils/* ./Utils/
+
+# Wallpapers
+cp -r ~/wallpapers ./
+
+# Packages
+pacman -Qtdq | sudo pacman -Rns -
+pacman -Qqen > install/pacman_all.txt
+pip freeze > install/pip_install.txt
